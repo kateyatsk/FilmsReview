54b0ac7d25050c79170a2ee6ebaf8ea9ce3a3d90
kateyatsk <klaettoe2023@gmail.com>
2025-06-23 16:51:27 +0300

    Initial Commit

---
+++ FilmsReview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmsReview
+//
+//  Created by Екатерина Яцкевич on 23.06.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
